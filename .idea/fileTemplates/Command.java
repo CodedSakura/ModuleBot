2859969e78b07e0c71d7a1b9ae0671ae4cee0898
ThePhisics101 <thephisics101@gmail.com>
2018-02-08 19:45:01 +0200

    Add missed files

---
+++ .idea/fileTemplates/Command.java
@@ -0,0 +1,26 @@
+package modulebot.info;
+
+import modulebot.main.Command;
+import net.dv8tion.jda.core.entities.Message;
+
+public class ${NAME} extends Command {
+    @Override
+    public String getName() {
+        return "${NAME}";
+    }
+
+    @Override
+    public String getHelp() {
+        return "${INFO}";
+    }
+
+    @Override
+    public String[] getUsages() {
+        return new String[] {""};
+    }
+
+    @Override
+    public void run(Message m) {
+        
+    }
+}
